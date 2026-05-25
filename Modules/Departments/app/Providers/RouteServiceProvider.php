76674275429a8e306dfd7fe9161c846f795ba0c1
unknown <officialboss191@gmail.com>
2026-05-25 01:54:14 +0100

    fixed the admin issue on dep

---
+++ Modules/Departments/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Departments\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $name = 'Departments';
+
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    public function map(): void
+    {
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')->group(module_path($this->name, '/routes/web.php'));
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        // No API routes for this module
+    }
+}
